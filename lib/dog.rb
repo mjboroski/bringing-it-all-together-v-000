13ae8c48b6cb396ef52dd36a27969ddd794d82ad
Michael Boroski <mjboroski@gmail.com>
2018-05-22 20:46:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,7 +39,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    Dog.new(row[1],row[2],row[0])
+    name=row[1]
+    breed=row[2]
+    id=row[0]
+    Dog.new(id:id, name:name, breed:breed)
   end
 
   def self.find_by_name
