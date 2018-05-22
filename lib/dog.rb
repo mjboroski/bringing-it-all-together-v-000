eb60544235db09e496f324c7a1c932ea84906f97
Michael Boroski <mjboroski@gmail.com>
2018-05-22 20:40:11 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,60 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:,breed:,id:nil)
+    @name=name
+    @breed=breed
+    @id=id
+  end
+
+  def self.create_table
+    sql= <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+
+  def save
+
+  end
+
+  def self.create(name,breed)
+    dog = Dog.new(name, breed)
+  end
+
+  def self.find_by_id
+
+  end
+
+  def self.find_or_create_by(a,b)
+
+  end
+
+  def self.new_from_db
+    sql= <<-SQL
+      SELECT *
+      FROM dogs
+      )
+      SQL
+    DB[:conn].execute(sql).map do |row|
+      Dog.new(row[1],row[2],row[0])
+    end
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
+end
