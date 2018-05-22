984f0cbfd5051e572d692beade47aa7c37ddaa91
Michael Boroski <mjboroski@gmail.com>
2018-05-22 20:48:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,13 @@ class Dog
   end
 
   def self.find_by_name
-
+    sql= <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name=?
+      LIMIT 1
+      SQL
+    DB[:conn].execute(sql,@name)
   end
 
   def update
